ac6815b01ef9d064bdf2fe4c1090348a6a2cff11
Alessandro Manno <alessandro.manno@facile.it>
2018-11-07 09:12:40 +0100

    Updated init configuration file

--- libexec/effix/init.sh
+++ libexec/effix/init.sh
@@ -3,13 +3,15 @@
 _init () {
     cat > $PROJECT_DIR/$EFFIX_CONF <<EOL
 # One command each line
-# use [file] as file placeholder
-# You can use also a recipe. Use: effix recipes for more info
 
 # use CUSTOM_BIN_DIR="/custom/bin" if you want to use custom bin dir
+# You can use recipes. Use: effix recipes for more info
 
-vendor/bin/php-cs-fixer fix [file]
-vendor/bin/phpstan analyse -l 1 [file]
-vendor/bin/phpunit [file]
+phpunit
+phpstan
+csfix
+
+# You can also run a custom command using [file] as placeholder. Example:
+# your/script.sh [file]
 EOL
 }
